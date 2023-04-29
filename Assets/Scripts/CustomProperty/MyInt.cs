6c897ed3d63e7368c5440a87c5343cd1b7d9a65f
BlackHold2K <cuteza456@gmail.com>
2023-04-29 17:05:42 +0700

    Add CustomProperty and Adeptive Attack

---
+++ Assets/Scripts/CustomProperty/MyInt.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace CustomProperty
+{
+    [Serializable]
+    public class MyInt
+    {
+        public int intValue = 0;
+        public string stringValue;
+    }
+}
+
