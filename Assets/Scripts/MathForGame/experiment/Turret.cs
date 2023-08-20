2df83d65ef4a18645f98568f563c108d8384e065
Panit <cuteza456@gmail.com>
2023-08-21 00:29:17 +0700

    finish assignment 2

--- Assets/Scripts/MathForGame/experiment/Turret.cs
+++ Assets/Scripts/MathForGame/experiment/Turret.cs
@@ -8,18 +8,23 @@ public class Turret : MonoBehaviour
     public TurretTrigger turretTrigger;
     public Transform gunTransform;
 
-    void OnDrawGizmos() 
+    public float smoothingFactor = 1f;
+
+    void Update() 
     {
+        Quaternion targetRotation;
 
         if(turretTrigger.Contains(target.position))
         {
             //world space
             Vector3 vecToTarget = target.position - gunTransform.position;
-            gunTransform.rotation = Quaternion.LookRotation(vecToTarget,transform.up);
+            targetRotation = Quaternion.LookRotation(vecToTarget,transform.up);
         }
         else
         {
-            gunTransform.rotation = Quaternion.Euler(Vector3.forward);    
+            targetRotation = Quaternion.LookRotation(transform.forward,transform.up);
         }
+        
+            gunTransform.rotation = Quaternion.Slerp(gunTransform.rotation,targetRotation,smoothingFactor * Time.deltaTime);
     }
 }
