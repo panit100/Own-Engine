8d39f5e895d658b41728bba3fac6b6822ed072ba
Panit <cuteza456@gmail.com>
2023-08-23 14:16:06 +0700

    angle deg 2 red

--- Assets/Scripts/Test/Planet.cs
+++ Assets/Scripts/Test/Planet.cs
@@ -8,9 +8,46 @@ public class Planet : MonoBehaviour
     public float offset;
 
     public float rotateSpeed;
+    float radian = 0;
+    float degrees = 0;
     float angle = 0;
 
     public float waitingTime;
+
+    //TODO: Rotate Planet
+
+    // void Update()
+    // {
+    //     if(waitingTime > 0)
+    //     {
+    //         waitingTime -= Time.deltaTime;
+    //         return;
+    //     }
+
+    //     if (subPlanet == null)
+    //         return;
+        
+    //     RotatePlanet();
+
+    //     SubPlanetOrbit();
+    // }
+
+    // void RotatePlanet()
+    // {
+    //     radian += rotateSpeed * Time.deltaTime;
+
+    //     degrees = radian * Mathf.Rad2Deg;
+
+    //     transform.rotation = Quaternion.Euler(0,degrees,0);
+    // }
+
+    // void SubPlanetOrbit()
+    // {
+    //     Vector3 subPlanetPos = (transform.TransformDirection(transform.forward) * offset) + transform.position ;
+    //     subPlanet.position = subPlanetPos;
+    // }
+
+    //TODO: Change Planet Position
 
     void Update()
     {
@@ -22,22 +59,15 @@ public class Planet : MonoBehaviour
 
         if (subPlanet == null)
             return;
-        
-        RotatePlanet();
 
-        SubPlanetOrbit();
-    }
-
-    void RotatePlanet()
-    {
         angle += rotateSpeed * Time.deltaTime;
-
-        transform.rotation = Quaternion.Euler(0,angle,0);
+    
+        SubPlanetPosition();
     }
 
-    void SubPlanetOrbit()
+    void SubPlanetPosition()
     {
-        Vector3 subPlanetPos = (transform.TransformDirection(transform.forward) * offset) + transform.position ;
-        subPlanet.position = subPlanetPos;
+        Vector3 direction = new Vector3(Mathf.Cos(angle),0,Mathf.Sin(angle));
+        subPlanet.position = transform.position + (direction * offset);
     }
 }
