71b94dd9dfd14ffb29856c11b5dcc2f731501d05
Panit <cuteza456@gmail.com>
2024-03-24 15:52:08 +0700

    update test class

--- Assets/Scripts/Utilities/Json_CSV/JSON/TestJsonClass.cs
+++ Assets/Scripts/Utilities/Json_CSV/JSON/TestJsonClass.cs
@@ -2,18 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using CuteEngine.Utilities.Converter;
-using Unity.VisualScripting;
 using UnityEngine;
 
 [Serializable]
 public class TestClass
 {
 
-    public string text { get; set; }
-    public int num { get; set; }
-    public float num2 { get; set; }
-    public bool toggle { get; set; }
+    public string text { get { return Text; } set { Text = value; } }
+    public int num { get { return Num; } set { Num = value; } }
+    public float num2 { get { return Num2; } set { Num2 = value; } }
+    public bool toggle { get { return Toggle; } set { Toggle = value; } }
 
+    public string Text;
+    public int Num;
+    public float Num2;
+    public bool Toggle;
 
     public void SaveData(string fileName)
     {
