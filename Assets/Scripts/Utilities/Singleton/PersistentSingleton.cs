4f07367fc954cdfb1fb19182009c084d47d9092e
Panit <cuteza456@gmail.com>
2024-03-27 21:19:44 +0700

    refactor singleton base class

---
+++ Assets/Scripts/Utilities/Singleton/PersistentSingleton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using CuteEngine.Utilities;
+using UnityEngine;
+
+public class PersistentSingleton<T> : Singleton<T> where T : Singleton<T>
+{
+    protected override void InitAfterAwake()
+    {
+
+    }
+
+    protected override void Init()
+    {
+        base.Init();
+        if (Application.isPlaying)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
